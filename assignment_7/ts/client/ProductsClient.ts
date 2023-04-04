baaac72f68714290a814fb12e28cb33d51098415
Marcus Kalmar <marcus@videvit.se>
2023-04-04 16:55:21 +0200

    update solution

--- assignment_7/ts/client/ProductsClient.ts
+++ assignment_7/ts/client/ProductsClient.ts
@@ -1,14 +1,35 @@
-const axios = require('axios').default
+import axios, { AxiosResponse } from 'axios';
+import { Product } from '../server/models/product';
 
-const BASE_URL = "http://localhost:8008"
+const API_URL = 'http://localhost:8008';
 
-// Get all products
+export const getAllProducts = async (): Promise<Product[]> => {
+    const response: AxiosResponse<Product[]> = await axios.get(`${API_URL}/products`);
+    return response.data;
+};
 
-export const getProducts = async function () {
-    const response = await axios.get(BASE_URL + '/products') // String concatenation for complete URL
+export const getProductById = async (id: number): Promise<Product | null> => {
+    try {
+        const response: AxiosResponse<Product> = await axios.get(`${API_URL}/products/${id}`);
+        return response.data;
+    } catch (error: any) {
+        if (error.response.status === 404) {
+            return null;
+        }
+        throw error;
+    }
+};
 
-    const products = response.data
+export const createProduct = async (product: Omit<Product, 'id'>): Promise<Product> => {
+    const response: AxiosResponse<Product> = await axios.post(`${API_URL}/products`, product);
+    return response.data;
+};
 
-    console.log(products)
+export const updateProduct = async (product: Product): Promise<Product> => {
+    const response: AxiosResponse<Product> = await axios.put(`${API_URL}/products/${product.id}`, product);
+    return response.data;
+};
 
-}
+export const deleteProduct = async (id: number): Promise<void> => {
+    await axios.delete(`${API_URL}/products/${id}`);
+};
--- assignment_7/ts/client/index.ts
+++ assignment_7/ts/client/index.ts
@@ -1,6 +1,16 @@
-import { getProducts } from "./ProductsClient";
+import * as ProductsCLient from "./ProductsClient";
 
 // Different from js file, can you explain in your own words why?
 
-getProducts()
+
+ProductsCLient.createProduct({ name: "Salt", price: 4000 })
+ProductsCLient.createProduct({ name: "Pepper", price: 2000 })
+
+ProductsCLient.getAllProducts()
+ProductsCLient.getProductById(1)
+ProductsCLient.getProductById(2)
+
+ProductsCLient.updateProduct({ id: 1, name: "Nope", price: 3000 })
+ProductsCLient.deleteProduct(2)
+ProductsCLient.getAllProducts()
 
